7ba540c5d893a86401f7e079be02d3224a5e1150
vincent.sue <gnux123@gmail.com>
2016-04-20 15:36:45 +0800

    finished post data.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var request = require("request");
 var	cheerio = require('cheerio');
 var	express = require("express");
 var fs = require('fs');
+var bodyParser = require('body-parser');
 // var schedule = require("node-schedule");
 var	superagent = require("superagent");
 var	home = require("./home");
@@ -15,11 +16,15 @@ var apiUri = "http://www.newegg.com.tw/";
 var yelpApiUrl = "https://api.yelp.com/v2/search/?location=taipei&cc=TW&lang=zh";
 var secret = "&oauth_consumer_key=xdU_aD0Qq4SnaWWJLkJnRQ&oauth_consumer_secret=75D1ZjvYYk1KhZD44byjldr-CF8&oauth_token=C8ODZt0QETpQFqDUTMBwWefIuEGVeSZ3&oauth_token_secret=mZsjhFUJSB51KWIp9Rux-gNqq60&oauth_signature_method=HMAC-SHA1&oauth_signature=&oauth_timestamp=&oauth_nonce=";
 
+//post json parser
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //cors
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 //images and js folder
@@ -29,7 +34,7 @@ app.use('/img/Activity', express.static(__dirname + '/app/public'));
 
 //client UI design
 router.get('/',function(req,res){
-    res.sendfile(__dirname + '/app/index.html');
+    res.sendfile(__dirname + '/app/test.html');
 });
 
 
@@ -53,22 +58,23 @@ router.get('/getData/:itemID',function(req,res){
 });
 
 //postData
-router.get('/postData/',function(req,res){
-    res.setHeader('Content-Type', 'application/json');
+router.post('/postData/',function(req,res){
+    // res.setHeader('Content-Type', 'application/json');
     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:50118');
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     // res.setHeader('Access-Control-Allow-Credentials', true);
     // // var items = req.params.itemID;
     // // var itemsDetail = [];
+    var dataKey = Object.keys(req.body)[0];
+
     request({
         url: "http://localhost:50118/api/Item/getItemDetailByItemIds",
         // url: apiUri+"/api/Item/getItemDetailByItemId?itemid="+items,
         method: "POST",
-        json: ["482308","432981"]
+        json: req.body[""+dataKey+""]
     }, function(e,r,b) {
         if(!e) {
-            // b = b.replace(/\\/g,"");
             res.send(b);
         }
     });
